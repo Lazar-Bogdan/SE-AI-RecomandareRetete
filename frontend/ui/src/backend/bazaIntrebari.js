8c9ec34bfcd47aa2de7524511d237442e7161094
Lazar Bogdan <bogdilazar5@gmail.com>
2023-03-17 08:49:18 +0200

    adding the first question

--- frontend/ui/src/backend/bazaIntrebari.js
+++ frontend/ui/src/backend/bazaIntrebari.js
@@ -1,27 +1,18 @@
 export const questions = [
     {
-        question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
+        question: "Are you vegan?"
     }
 ]
 
 export const questionsOneYes = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsOneNo = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
@@ -37,63 +28,42 @@ export const questionsTwoYes = [
 export const questionsTwoNo = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsThreeYes = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsThreeNo = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsFourYes = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsFourNo = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsFiveYes = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
 export const questionsFiveNo = [
     {
         question: "WHY IS THIS GREEN"
-    },
-    {
-        question: "WHY IS THIS PURPULE"
     }
 ]
 
